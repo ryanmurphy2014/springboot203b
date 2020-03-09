3a3ec9fd92e7cbc8c1c19092b9c8b539fc7fb0aa
GBTC440013UR <ryanmurphy2014@gmail.com>
2020-03-09 18:59:40 -0400

    Initial commit

---
+++ src/main/java/com/rocktype/springboot203b/Springboot203bApplication.java
@@ -0,0 +1,13 @@
+package com.rocktype.springboot203b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot203bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot203bApplication.class, args);
+    }
+
+}
